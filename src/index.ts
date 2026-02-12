b636f963db89190d22249805d949e330a726540d
Alex-Evo-2266 <ghbdtn2266@mail.ru>
2026-02-12 20:47:41 +0300

    imports

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,9 @@
 export type { UserMeData, Role, Privilege, AuthState } from "./lib/types";
 
 export {type AuthContextValue, AuthContext} from "./lib/AuthContext"
+
+export {AuthGuard} from './lib/AuthGuard'
+export {AuthProvider} from './lib/AuthProvider'
+export {authFetch} from './lib/authFetch'
+export {useAuth} from './lib/useAuth'
+export {useHasPrivilege} from './lib/useHasPrivilege'
