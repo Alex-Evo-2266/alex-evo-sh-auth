69f3bc9138949e45e26a480b25bd01b8d8a60630
Alex-Evo-2266 <ghbdtn2266@mail.ru>
2026-02-12 20:19:26 +0300

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dts from "vite-plugin-dts"
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/alex-evo-sh-ui-kit/",  
+  base: "/alex-evo-sh-auth/",  
   plugins: [
     react(),
     dts({ insertTypesEntry: true })
   ],
+  build: {
+    lib:{
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: "alex-evo-sh-auth",
+      formats: ["es", "umd"],
+      fileName: (format)=> `alex-evo-sh-sh-auth.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  }
 })
